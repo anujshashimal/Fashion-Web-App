876a28d4007d3df7e3f767ba6cee4d958a35c2f9
lahirulakruwan <lahirulakruwan5@gmail.com>
2020-04-04 22:02:54 +0530

    some modification on routes files

--- api/routes/Products.js
+++ api/routes/Products.js
@@ -3,12 +3,12 @@ const upload  = require('../Uploadmiddleware/upload');
 const ProductControlller = require('../controllers/Product');
 
 
-router.get('/:id',ProductControlller.Find_Product);       // find the products
+router.get('/find/:stockmanagerid',ProductControlller.Find_Product);       // find the products
 
 router.post('/add',upload.single('image'),ProductControlller.Store_Product);     //  Add the product
 
 router.post('/update/:id',upload.single('image'),ProductControlller.Update_Product); // update  the product
 
-router.delete('/delete/:id',ProductControlller.Delete_Product);    // Delete Product
+router.delete('/delete/:productid',ProductControlller.Delete_Product);    // Delete Product
 
 module.exports = router;
