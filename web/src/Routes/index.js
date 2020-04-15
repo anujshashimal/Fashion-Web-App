8f0fdccabfb7be7062a51abd99236cd21dd91789
unknown <anujshashimal456@gmail.com>
2020-04-16 01:58:34 +0530

    React Route Configuring done

--- web/src/Routes/index.js
+++ web/src/Routes/index.js
@@ -1,1 +1,79 @@
-git 
+import React , {Component} from 'react';
+import { BrowserRouter, Route} from "react-router-dom";
+{/*Lahiru Import Here*/}
+
+
+
+
+
+
+
+{/*Vishaka Import Here*/}
+
+
+
+
+
+
+
+
+{/*Ravindu Import Here*/}
+
+
+
+
+
+
+
+
+{/*Anuj Import Here*/}
+
+
+
+
+
+
+
+
+{/*END*/}
+
+
+
+function Router() {
+    return (
+        <BrowserRouter>
+            {/*Anuj*/}
+            <Route path = "/Sample" component={sampleRoute} />
+
+
+
+
+
+            {/*Components Path Goes to here*/}
+            {/*Lahiru*/}
+
+
+
+
+
+            {/*Vishaka*/}
+
+
+
+
+
+
+            {/*Ravindu*/}
+
+
+
+
+
+            {/*Others*/}
+
+
+        </BrowserRouter>
+    );
+}
+
+export default Router;
