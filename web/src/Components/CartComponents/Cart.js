593b86355b88d518d750a03000518c3ccfde5c23
unknown <anujshashimal456@gmail.com>
2020-04-16 21:58:10 +0530

    Cart Update

--- web/src/Components/CartComponents/Cart.js
+++ web/src/Components/CartComponents/Cart.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {addBasket} from "../../Actions/addActions";
+import {connect} from 'react-redux';
 
+const Cart = (props) =>{
+    console.log(props)
 
-function cart(){
+    const [numberBasket, settnumberBacket] = useState(0);
+
+    const addToBasket = () => {
+        settnumberBacket(numberBasket +1);
+
+    }
+
     return (
+        <div>
         <header>
             <p>Hello I'm Cartt</p>
-            <button type="button" className="btn btn-denger">Add to Cart</button>
-        </header>
+            <button type="button" className="btn btn-primary"onClick={addToBasket}>Add to Cart</button>
 
+            <h1> Current Numbers in Cart {numberBasket}</h1>
+        </header>
+        </div>
 
 
     )
@@ -15,4 +28,4 @@ function cart(){
 
 }
 
-export default cart;
+export default connect(null, {addBasket})(Cart);
--- web/src/store.js
+++ web/src/store.js
@@ -1,7 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from 'redux-thunk';
+import rootReducer from './Reducers'
+
+const initialState = {};
+const middleware = [thunk]
+
 export default function configureStore() {
     return createStore(
-        applyMiddleware(thunk)
+        rootReducer,
+        initialState,
+        composeWithDevTools(applyMiddleware(...middleware))
     );
 }
