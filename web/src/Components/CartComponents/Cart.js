f63be308683dc5a85e685e568093388d79960da5
unknown <anujshashimal456@gmail.com>
2020-04-16 20:58:10 +0530

    Adding some bootstrap

--- web/src/Components/CartComponents/Cart.js
+++ web/src/Components/CartComponents/Cart.js
@@ -4,7 +4,7 @@ import React from 'react';
 function cart(){
     return (
         <header>
-            <p>Hello I'm Cart</p>
+            <p>Hello I'm Cartt</p>
             <button type="button" className="btn btn-denger">Add to Cart</button>
         </header>
 
